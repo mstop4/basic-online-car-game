a6ada1ff31f302bd741adc012a63093c9c99bdc6
mstop4 <mstop4@gmail.com>
2018-01-17 11:30:05 -0500

    added newPlayer.js

--- client/state/utils/index.js
+++ client/state/utils/index.js
@@ -1,5 +1,5 @@
-export const isDown = {game, key} => game.input.keyboard.isDown(key)
-export const createText = {game, target} =>
+export const isDown = (game, key) => game.input.keyboard.isDown(key)
+export const createText = (game, target) =>
   game.add.text(target.x, target.y, '', {
     fontSize: '12px',
     fill: '#FFF',
